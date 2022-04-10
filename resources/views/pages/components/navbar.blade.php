e99e38b0fb1e6e64d067627fd49e5ca87897eafc
fahmyxfiles <fahmy.xfiles@gmail.com>
2022-04-11 01:19:11 +0700

    Update navbar.blade.php

--- resources/views/pages/components/navbar.blade.php
+++ resources/views/pages/components/navbar.blade.php
@@ -6,7 +6,7 @@
     <div class="container-fluid p-0">
         <!-- logo -->
         <a class="navbar-brand" href="{{route('web.home')}}">
-          <img src="{{ asset('storage') }}/@setting('app.logo')" alt="logo">
+          <img src="{{ asset('storage') }}/@setting('app.logo')" alt="logo" style="max-height: 40px;">
         </a>
         <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
         <span class="fa fa-bars"></span>
